ad4b5f128df6a9f11433d92926a5d2cc29c790ea
tom-gordon13 <tombcgordon@gmail.com>
2022-06-19 20:50:49 -0600

    Add more icons to skills page

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -16,7 +16,7 @@ export default function Skills() {
             </div>
 
             <div className="skill-container">
-                <h1 className='secondary-header'>Tools/Frameworks</h1>
+                <h1 className='secondary-header'>Application Frameworks/Database Tools</h1>
                 <div className="skill-list">
                     <i className="devicon-react-original icon"><h3>React</h3></i>
                     <i className="devicon-nodejs-plain icon"><h3>Node.JS</h3></i>
@@ -24,8 +24,19 @@ export default function Skills() {
                     <i className="devicon-express-original icon"><h3>Express</h3></i>
                     <i className="devicon-django-plain icon"><h3>Django</h3></i>
                     <i className="devicon-postgresql-plain icon"><h3>PostgreSQL</h3></i>
-          
+                    {/* <i className="devicon-microsoftsqlserver-plain icon"><h3>MS SQL Server</h3></i> */}
+                </div>
 
+                <div className="skill-container">
+                    <h1 className='secondary-header'>Additional Tools</h1>
+                    <div className="skill-list">
+                        <i className="devicon-github-original icon"><h3>GitHub</h3></i>
+                        <i className="devicon-linux-plain icon"><h3>Linux</h3></i>
+                        <i className="devicon-bootstrap-plain icon"><h3>Bootstrap</h3></i>
+                        <i className="devicon-figma-plain icon"><h3>Figma</h3></i>
+                        <i className="devicon-heroku-original icon"><h3>Heroku</h3></i>
+
+                    </div>
                 </div>
             </div>
         </div>
