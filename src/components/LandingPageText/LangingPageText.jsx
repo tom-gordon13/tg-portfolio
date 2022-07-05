b05fffe02296daddcfd1160fac00b79f95fe952b
tom-gordon13 <tombcgordon@gmail.com>
2022-07-05 12:00:21 -0600

    Add wave styling to home page

--- src/components/LandingPageText/LangingPageText.jsx
+++ src/components/LandingPageText/LangingPageText.jsx
@@ -26,18 +26,25 @@ export default function LandingPage() {
 
     return (
         <>
+
             <div className='main-container d-flex flex-column align-items-start'>
+
                 <CSSTransition in={showTest} timeout={1000} classNames={{
-                    enterActive: 'animate__flipInX',
-                    exitActive: 'animate__bounceOut'
+                    enterActive: 'animate__fadeIn',
+                    exitActive: 'animate__fadeOut'
                 }}
                     onEnter={showBlock}
                     onEntered={removeOpacity}
-                    // onExited={hideTest}
-                    className={`animate__animated ${testClasses} main-header`}>
-                    <h1 className='main-header'>Tom Gordon</h1>
+                    className={`animate__animated ${testClasses} `}>
+                   <section>
+                    <div className="content">
+                        <h2>Tom Gordon</h2>
+                        <h2>Tom Gordon</h2>
+                    </div>
+                </section>
                 </CSSTransition>
-
+                
+                <div className="second-container">
                 <CSSTransition in={showTest} timeout={1000} classNames={{
                     enterActive: 'animate__bounceInLeft',
                     exitActive: 'animate__bounceOut'
@@ -48,8 +55,8 @@ export default function LandingPage() {
                     className={`animate__animated ${testClasses} secondary-header`}>
                     <h1>Full-Stack Software Engineer</h1>
                 </CSSTransition>
-                
-                
+
+
 
 
                 <CSSTransition in={showTest} timeout={3000} classNames={{
@@ -60,7 +67,7 @@ export default function LandingPage() {
                     onEntered={removeOpacity}
                     // onExited={hideTest}
                     className={`animate__animated ${testClasses} hr1`}>
-                    <hr className='hr1'/>
+                    <hr className='hr1' />
                 </CSSTransition>
 
 
@@ -74,7 +81,9 @@ export default function LandingPage() {
                     className={`animate__animated ${testClasses} tertiary-header`}>
                     <h1>I'm a full-stack software engineer living in Boulder, Colorado with a background in sales and financial analysis.</h1>
                 </CSSTransition>
+                </div>
             </div>
+
 
         </>
     )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import './NavBar.css'
 
 export default function NavBar({ currTop, linkClick }) {
     function handleClick(evt) {
-        const value = parseInt(evt.target.id)
+        const value = parseInt(evt.currentTarget.id)
         linkClick(window.innerHeight * value)
     }
 
@@ -17,23 +17,24 @@ export default function NavBar({ currTop, linkClick }) {
 
             </div>
             <div className="link-container d-flex flex-column">
-                <div className='link logo-holder' onClick={handleClick}>
-                    <img src="/iconmonstr-user-1.svg" alt="" className='nav-icon' id='1' key='1' />
-                    <span className="link-text" id='1' key='1'>About Me</span>
+                <div className='link logo-holder' id='1' key='1' onClick={handleClick}>
+                    <img src="/iconmonstr-user-1.svg" alt="" className='nav-icon' />
+                    <span className="link-text" >About Me</span>
                 </div>
-                <div className='link logo-holder' onClick={handleClick}>
-                    <img src="/iconmonstr-tools-14.svg" alt="" className='nav-icon' id='2' key='2' />
-                    <span className="link-text" id='2' key='2'>Skills</span>
+                
+                <div className='link logo-holder' id='2' key='2' onClick={handleClick}>
+                    <img src="/iconmonstr-tools-14.svg" alt="" className='nav-icon' />
+                    <span className="link-text">Skills</span>
                 </div>
-                &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-                <div className='link logo-holder' onClick={handleClick}>
-                    <img src="/iconmonstr-code-7.svg" alt="" className='nav-icon' id='3' key='3' />
-                    <span className="link-text" id='3' key='3'>Projects</span>
+                
+                <div className='link logo-holder' id='3' key='3' onClick={handleClick}>
+                    <img src="/iconmonstr-code-7.svg" alt="" className='nav-icon'  />
+                    <span className="link-text" >Projects</span>
                 </div>
-                &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-                <div className='link logo-holder' onClick={handleClick}>
-                    <img src="/iconmonstr-email-1.svg" alt="" className='nav-icon' id='4' key='4' />
-                    <span className="link-text" id='4' key='4'>Contact Me</span>
+                
+                <div className='link logo-holder' id='4' key='4' onClick={handleClick}>
+                    <img src="/iconmonstr-email-1.svg" alt="" className='nav-icon' />
+                    <span className="link-text" >Contact Me</span>
                 </div>
             </div>
 
